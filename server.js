0b62a4d66118c6bd164b53bc4896ebadccec89bc
saketh-05 <raisingsurya05@gmail.com>
2025-10-13 15:14:58 +0530

    ajay work

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("This works! with Docker + AWS EKS 🚀"));
+app.get("/", (req, res) =>
+  res.send(
+    "This is DevOps Assignment 2 - Dockerizing a Node.js Application using Dockerfile"
+  )
+);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
